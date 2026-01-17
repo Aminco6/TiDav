e2e1d27d2383ea82db163acd1ae8725283a60209
Aminco6 <voicedontdie@gmail.com>
2026-01-17 21:03:51 +0100

    302error5

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
   
-    path('', views.home, name='home'),
+    path('', views.index, name='index'),
     path('signup/', views.signup_view, name='signup'),
     path('login/', views.login_view, name='login'),
     
