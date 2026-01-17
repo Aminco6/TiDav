0d0e91d1f88f09ab7960f99410ebe1d7f0e6b733
Aminco6 <voicedontdie@gmail.com>
2026-01-17 18:48:27 +0100

    s

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-ti-dav-secret-key-2024-change-this-in-production'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://tidav.artclash.com.ng', '127.0.0.1', 'www.tidav.artclash.com.ng', 'tidav.artclash.com.ng']
 
 # Application definition
 INSTALLED_APPS = [
